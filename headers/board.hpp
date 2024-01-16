85764314c01266078a123751eccc5dd9c399cecd
OPC-16 <chavan.p.omkar@gmail.com>
2024-01-16 16:55:15 +0530

    Create basic board for game

--- headers/board.hpp
+++ headers/board.hpp
@@ -2,5 +2,19 @@
 
 class Board {
     public:
+        Board(int height, int width) {
+            int yMax, xMax;
+            getmaxyx(stdscr, yMax, xMax);
+
+            board_win = newwin(height, width, (yMax/2)-height/2, (xMax/2)-width/2);
+            box(board_win, 0, 0);
+            wrefresh(board_win);
+
+            this->height = height;
+            this->width = width;
+        }
+
     private:
+        WINDOW *board_win;
+        int height, width;
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <ncurses.h>
 #include "headers/board.hpp"
 
+#define board_dim 18
+#define board_rows board_dim
+#define board_cols (board_dim * 2.5)
+
 int main() {
     initscr();
     refresh();
+
+    Board board(board_rows, board_cols);
 
     getch();
     endwin();
